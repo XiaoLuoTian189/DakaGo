16af6cad27aaca0c40160ecc1a7f5ffbf8df796f
小洛天 <wanghaotianscimir@gmail.com>
2025-11-17 21:32:14 +0800

    Add files via upload

--- migrations/2024_01_01_000001_add_checkin_type_to_discussions.php
+++ migrations/2024_01_01_000001_add_checkin_type_to_discussions.php
@@ -1,12 +1,11 @@
 <?php
 
 use Flarum\Database\Migration;
-use Illuminate\Database\Schema\Blueprint;
 
 return Migration::addColumns(
     'discussions',
-    function (Blueprint $table) {
-        $table->boolean('is_checkin_type')->default(0);
-    }
+    [
+        'is_checkin_type' => ['type' => 'boolean', 'default' => 0],
+    ]
 );
 
